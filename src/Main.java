c6a68b4cc9a0d9c6fd5326a526ff5302dab199df
kml_c_17 <kocenkotars@gmail.com>
2017-02-16 11:40:31 +0200

    add stringl

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
             for (int g = 0; g < 10; g++) {
             if (g%2==0){
-                System.out.println("g = " + g);
+                System.out.println("asffsag = " + g);
             }
 
 
