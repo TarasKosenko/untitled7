168369efb4036040775445ad7eb8743b3227710e
kml_c_17 <kocenkotars@gmail.com>
2017-02-16 11:28:56 +0200

    main Commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+public class Main {
+
+    public static void main(String[] args) {
+            for (int g = 0; g < 10; g++) {
+            if (g%2==0){
+                System.out.println("g = " + g);
+            }
+
+
+        }
+    }
+}
